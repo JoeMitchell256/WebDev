3e9a63e4dc8f06a7294037c2049f335cc144122e
Joseph Mitchell <70342721+JoeTheBro256@users.noreply.github.com>
2021-07-23 16:33:07 -0400

    Add php to ensure user input is being processed
    Used for python arg parsing later on in development process

--- process.php
+++ process.php
@@ -1,9 +1,14 @@
 <?php
-
-function pre_r( $array ){
-  echo '<pre>';
-  print_r($array);
-  echo '</pre>';
-}
+  pre_r($_POST);
+  if (isset($_POST['submit'])){
+    if ( (!isset($_POST['pulsefunction'])) || (!isset($_POST['n'])) ){
+      $error = "*" . "Please fill all required fields";
+    }
+    else{
+      $pulsefunction = $_POST['pulsefunction'];
+      $n = $_POST['n'];
+      echo"<h1>INPUT RECIEVED</h1><br>";
+    }
+  }
 
 ?>
