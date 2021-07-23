0763b538b3ede7aa7225dcb981f9862675f96a88
Joseph Mitchell <70342721+JoeTheBro256@users.noreply.github.com>
2021-07-23 16:42:47 -0400

    Update process.php

--- process.php
+++ process.php
@@ -1,12 +1,12 @@
 <?php
-  pre_r($_POST);
-  if (isset($_POST['submit'])){
-    if ( (!isset($_POST['pulsefunction'])) || (!isset($_POST['n'])) ){
+  pre_r($_GET);
+  if (isset($_GET['submit'])){
+    if ( (!isset($_GET['pulsefunction'])) || (!isset($_GET['n'])) ){
       $error = "*" . "Please fill all required fields";
     }
     else{
-      $pulsefunction = $_POST['pulsefunction'];
-      $n = $_POST['n'];
+      $pulsefunction = $_GET['pulsefunction'];
+      $n = $_GET['n'];
       echo"<h1>INPUT RECIEVED</h1><br>";
     }
   }
