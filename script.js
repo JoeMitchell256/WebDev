0744b558fa3837bdb586a1d9c1b46129afd4400c
Joseph Mitchell <70342721+JoeTheBro256@users.noreply.github.com>
2021-07-23 20:13:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-var name = window.prompt("What is your name?");
+var fourier_coefficients = window.prompt("Please enter fourier coefficients (a_0, a_1, a_2, b_1, b_2): ");
+var n = window.prompt("Please enter a value of n: ");
+
+
