d1c5fdde58da7aebfd6ad4c9017ab5f81b96b9a1
Joseph Mitchell <70342721+JoeTheBro256@users.noreply.github.com>
2021-07-23 23:22:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var pulse_function = window.prompt("Please enter a pulse function (4 values (0 or 1) from 0 to 2pi): ");
 var n = window.prompt("Please enter a value of n: ");
 
-var canvas = createCanvas(100,100);
-canvas.position(0,0);
+var canvas = createCanvas(400,400);
