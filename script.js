0e21a6b73fd84ca4e995285cae23a5add8b839f0
Joseph Mitchell <70342721+JoeTheBro256@users.noreply.github.com>
2021-07-23 20:27:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var fourier_coefficients = window.prompt("Please enter fourier coefficients (a_0, a_1, a_2, b_1, b_2): ");
+var pulse_function = window.prompt("Please enter a pulse function (4 values (0 or 1) from 0 to 2pi): ");
 var n = window.prompt("Please enter a value of n: ");
 
 
