8acd2e6cd6ff9de1d6d0b37497d8ce6a808f1d0f
Joseph Mitchell <70342721+JoeTheBro256@users.noreply.github.com>
2021-07-23 23:00:09 -0400

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-file1 = open("testFile.txt", "w");
-file1.write(sys.argv[0])
-file1.close()
-
-#This program should write something to testFile.txt
-#This should help debug whether or not we are using php get action correctly!
