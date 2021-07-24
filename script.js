176bfe81c9ed57d6e5720cb8aab154948ec576c3
Joseph Mitchell <70342721+JoeTheBro256@users.noreply.github.com>
2021-07-24 00:06:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
 var pulse_function = window.prompt("Please enter a pulse function (4 values (0 or 1) from 0 to 2pi): ");
 var n = window.prompt("Please enter a value of n: ");
-
-function setup(){
-  createCanvas(400,400);
-  createElement('h1', 'Lucky Numbers');
-}
-
-function mousePressed(){
-  createP("My lucky number is: " + random(0,10));  
-}
-
-function draw(){
-  background(220);
-  fill(255,0,0);
-  rect(200,200,50,50);
-}
