93bc0554e9a0a43739ca817424a2bdf29bcf6fa9
Joseph Mitchell <70342721+JoeTheBro256@users.noreply.github.com>
2021-07-26 00:21:22 -0400

    Remove meaningless javascript file

--- script.js
+++
@@ -1,8 +0,0 @@
-var pulse_function = window.prompt("Please enter a pulse function (4 values (0 or 1) from 0 to 2pi): ");
-var n = window.prompt("Please enter a value of n: ");
-
-var c = getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();
